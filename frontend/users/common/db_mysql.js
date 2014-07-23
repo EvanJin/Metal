355a57489fa4d082125feec2dc4b9bb72972f512
Fontaine <jinbojin@qvod.com>
2014-07-23 18:09:35 +0800

    modify

--- frontend/users/common/db_mysql.js
+++
@@ -1,9 +0,0 @@
-var config = require('../config/mysql/config');
-var Sequelize = require('sequelize');
-
-var seq = new Sequelize(config.db_name, config.user, config.password, {
-  host: config.host,
-  port: config.port
-});
-
-module.exports = seq;
